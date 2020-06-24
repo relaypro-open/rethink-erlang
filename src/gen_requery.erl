e7a8d600e5d773f83903d60e78589607252f4274
Drew Gulino <dgulino@republicwireless.com>
2020-06-24 14:14:49 -0400

    increased reconnect wait to ensure it doesn't reconnect before db stops.

--- src/gen_requery.erl
+++ src/gen_requery.erl
@@ -6,7 +6,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
          code_change/3]).
 
--define(ReconnectWait, 200).
+-define(ReconnectWait, 1000).
 -define(ReconnectMax, 60000).
 -define(ReqlTimeout, infinity).
 
