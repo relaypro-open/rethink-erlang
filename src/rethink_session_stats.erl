ce759ad833564157860db536ab4a86f883ed5706
Jesse Stimpson <jstimpson@republicwireless.com>
2020-11-19 13:00:07 -0500

    await connection in pool

--- src/rethink_session_stats.erl
+++ src/rethink_session_stats.erl
@@ -1,6 +1,6 @@
 -module(rethink_session_stats).
 
--export([get_connections/1]).
+-export([get_connections/1, await_connections/2]).
 
 %% @doc returns a list of connection pids, sorted by
 %% their likelihood for being able to serve a new query
@@ -30,4 +30,29 @@ get_connections(Table) ->
       List),
     Pids = [ X || {X, _, _, _} <- List2 ],
     lists:filter(fun is_process_alive/1, Pids).
+
+%% @doc checks the ets table with get_connections/1, but if it returns
+%% empty, we try again on an aggressive exponential backoff until the
+%% timeout value is reached. (infinity not supported)
+await_connections(Table, Timeout) ->
+    await_connections(Table, Timeout*1000, 0).
+
+await_connections(Table, TimeoutU, Depth) ->
+    case get_connections(Table) of
+        [] ->
+            WaitU = trunc(100*math:pow(2, Depth)),
+            if WaitU < 1000 ->
+                   ok;
+               true ->
+                   timer:sleep(WaitU div 1000)
+            end,
+            TimeoutU2 = TimeoutU-WaitU,
+            if TimeoutU2 < 0 ->
+                   erlang:error(timeout);
+               true ->
+                   await_connections(Table, TimeoutU-WaitU, Depth+1)
+            end;
+        Connections ->
+            Connections
+    end.
 
